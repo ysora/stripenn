93ddb728b0e26f2701921393a4880ec20fd7df7f
ysora <ysora90@gmail.com>
2020-09-25 11:41:02 -0400

    created .gitignore

---
+++ src/stripenn.py
@@ -0,0 +1,10 @@
+import argparse
+
+def argumentParser():
+    parser = argparse.ArgumentParser(description='Stripenn')
+    parser.add_argument()
+
+def main():
+
+if __name__ == "__main__":
+    main()
