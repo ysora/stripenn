8b25318b3b8ee995af5cdae4c64e2506ccb7e993
ysora <ysora90@gmail.com>
2020-11-04 18:40:29 -0500

    commit

--- setup.py
+++ setup.py
@@ -7,25 +7,22 @@ README = (HERE / "README.md").read_text()
 
 setuptools.setup(
     name = 'stripenn',
-    version = '1.0.4',
+    version = '1.0.7',
+    author = 'Sora Yoon',
+    author_email = 'sora.yoon@pennmedicine.upenn.edu',
     description = "Image-processing based detection of architectural stripes from chromatic conformation data",
     long_description=README,
     long_description_content_type="text/markdown",
-    scripts = ['src/stripenn.py'],
-    author = 'Sora Yoon',
-    author_email = 'sora.yoon@pennmedicine.upenn.edu',
-    url = 'https://github.com/ysora/stripenn-python',
-    packages=setuptools.find_packages(),
+    url = 'https://github.com/ysora/stripenn',
+    packages_dir={'':'src'}
+    packages=setuptools.find_packages('src'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    include_package_data=False,
     install_requires=["cooler","pandas","numpy","matplotlib","opencv-python","scikit-image","scipy","joblib","tqdm","Bottleneck"],
     entry_points={
-    	"console_script": [
-    		"stripenn=src.stripenn:main"
-    	]
+    	"console_script": ["stripenn=stripenn.stripenn:main"]
     },
 )
