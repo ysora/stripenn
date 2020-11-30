fe6921083ed87a0355ae2aa17c638efed9027124
Sora Yoon (윤소라) <sora.yoon@pennmedicine.upenn.edu>
2020-11-30 18:10:43 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+
+stripenn is a command line interface python package developed for detection of atchitectural stripes from 3D genome conformation data. For installation and usage, please refer to the github page (https://github.com/ysora/stripenn)
--- setup.py
+++ setup.py
@@ -7,22 +7,22 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name = 'stripenn',
-    version = '1.0.23',
+    version = '1.1.5',
     author = 'Sora Yoon',
     author_email = 'sora.yoon@pennmedicine.upenn.edu',
     description = "Image-processing based detection of architectural stripes from chromatic conformation data",
     long_description=README,
     long_description_content_type="text/markdown",
     url = 'https://github.com/ysora/stripenn',
-    package_dir={'': 'src'},
-    packages=['stripenn'],
+    #package_dir={'': 'src'},
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["cooler","pandas","numpy","matplotlib","opencv-python","scikit-image","scipy","joblib","tqdm"],
+    install_requires=["cooler","pandas","numpy","matplotlib","opencv-python","scikit-image","scipy","joblib","tqdm",'typer','pathlib'],
     entry_points={
-    	"console_scripts": ["stripenn=stripenn.stripenn:main"]
+    	"console_scripts": ["stripenn=stripenn.cli:main"]
     },
 )
